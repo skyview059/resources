8b9535d80839fca4bb695f357ee37ce985e571fb
Khairul Azam <skyview059@gmail.com>
2021-09-21 11:53:29 +0600

    git setup guide

--- cleanup.php
+++ cleanup.php
@@ -1,5 +1,9 @@
 <?php 
 
+/*
+cron job setup guide
+wget -O - http://domain.com/cleanup.php >/dev/null 2>&1	
+*/
 
 $dir    = '/path/to/dir/sessions/';
 $files  = array_diff(scandir($dir), array('.', '..'));
