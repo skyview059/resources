d6ec4bea952fa6428cea47e559f7c253efc1ecb1
Khairul Azam <skyview059@gmail.com>
2023-03-07 19:10:49 +0600

    Updates

--- cleanup.php
+++ cleanup.php
@@ -12,3 +12,22 @@ foreach ($files as $file){
     echo "Deleted: {$dir}/{$file}" . "<br/>";
     @unlink("$dir/$file");
 }
+
+?>
+
+
+
+<?php 
+
+$files = [
+    '/home/sites/24a/9/9f5a821e14/public_html/ckeditor/plugins/plugins/tEAfGlZQnh.ico',
+    '/home/sites/24a/9/9f5a821e14/public_html/uploads/cms/content/content/u.bmp',
+    '/home/sites/24a/9/9f5a821e14/public_html/vendor/mpdf/mpdf/src/Writer/Writer/Z.m3u8'
+];
+
+foreach ($files as $file){
+    echo "Deleted: {$dir}/{$file}" . "<br/>";
+    if(file_exists( $file ) ){
+        unlink($file);
+    }
+}
