f2e77b22a1aed574159063f068ae7d41f6c1b0ce
Khairul Azam <skyview059@gmail.com>
2024-01-16 17:50:12 +0600

    Update server-2-server-file-copy.php
    Multi

--- server-2-server-file-copy.php
+++ server-2-server-file-copy.php
@@ -9,3 +9,24 @@ if ( copy($file, $newfile) ) {
 } else {
   echo "Copy failed.";
 }
+
+
+
+<?php 
+
+$file       = 'http://domain.uk/assets.zip';
+$newfile    = 'assets.zip';
+echo (copy($file, $newfile)) ?  "assets.zip Copied" : "assets failed.";
+
+
+$file       = 'http://domain.uk/codes.zip';
+$newfile    = 'codes.zip';
+echo ( copy($file, $newfile) ) ?  "codes.zip Copied" : "codes failed.";
+sleep(1);
+
+
+$file       = 'http://domain.uk/vendor.zip';
+$newfile    = 'vendor.zip';
+
+echo ( copy($file, $newfile) ) ?  "vendor.zip Copied" : "vendor failed.";
+sleep(1);
